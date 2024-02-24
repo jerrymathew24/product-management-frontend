2f07aa954d0cbe0ba82ad08c4c3c0c86e3fa3925
PUMKIN24 <jer2ymatheww@gmail.com>
2024-02-24 15:07:38 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage.js';
 import ProductDetails from './pages/ProductDetails';
 import PageNotFound from './pages/PageNotFound';
+import SignUp from './pages/Auth/SignUp.js';
+import Login from './pages/Auth/Login.js';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/signUp' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/productDetails' element={<ProductDetails />} />
         <Route path='*' element={<PageNotFound />} />
 
---
+++ src/pages/Auth/SignUp.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaRegUser } from "react-icons/fa6";
+
+const SignUp = () => {
+  return (
+    <div className="grid grid-cols-10 gap-4 items-center">
+      <div class="col-span-4 bg-cyan-800 flex flex-col items-center justify-center h-screen text-white">
+        <h1 className='text-2xl font-bold'>Welcome Back!</h1>
+        <p>To keep connected with us please login with your personal info</p>
+        <button className='border border-white rounded-2xl text-white p-3 m-3 font-normal text-xs '>SIGN IN</button>
+      </div>
+
+      <div class="col-span-6 flex flex-col items-center justify-center h-screen">
+        <h1 class="text-center text-2xl font-bold text-yellow-500 mb-5">Create Account</h1>
+        <input type="text" placeholder="Name" className="col-span-1 mb-4 bg-slate-100 text-sm p-3 rounded-md" />
+        <input type="email" placeholder="Email" className="col-span-1 mb-4 bg-slate-100 text-sm p-3 rounded-md" />
+        <input type="password" placeholder="Password" className="col-span-1 mb-4 bg-slate-100 text-sm p-3 rounded-md" />
+        <button className='bg-yellow-500 rounded-2xl text-white p-3 font-normal text-xs '>SIGN UP</button>
+      </div>
+
+
+    </div>
+
+  )
+}
+
+export default SignUp
